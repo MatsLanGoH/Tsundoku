06e80a08db171a766c3feae47992fa3c7a444143
MatsLanGoH <mats.lan.pod@googlemail.com>
2017-06-16 21:35:21 +0900

    Added createUrl helper method.

--- app/src/main/java/com/sandocap/sirfelius/tsundoku/QueryUtils.java
+++ app/src/main/java/com/sandocap/sirfelius/tsundoku/QueryUtils.java
@@ -1,5 +1,11 @@
 package com.sandocap.sirfelius.tsundoku;
 
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
 /**
  * Helper methods related to querying the Google Books API and parsing the response JSON data.
  */
@@ -13,4 +19,19 @@ class QueryUtils {
     private QueryUtils() {}
 
 
+
+    /**
+     * Returns an {@link URL} from a String
+     * */
+    private URL createUrl(String stringUrl) {
+        URL url = null;
+
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error creating URL: ", e);
+        }
+
+        return url;
+    }
 }
