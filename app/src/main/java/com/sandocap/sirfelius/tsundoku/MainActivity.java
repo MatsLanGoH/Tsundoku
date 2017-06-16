5f9e9a757a0b6448006809af9aa23c599b8de45d
MatsLanGoH <mats.lan.pod@googlemail.com>
2017-06-16 16:38:02 +0900

    Reference TextView for initial testing.

--- app/src/main/java/com/sandocap/sirfelius/tsundoku/MainActivity.java
+++ app/src/main/java/com/sandocap/sirfelius/tsundoku/MainActivity.java
@@ -2,15 +2,24 @@ package com.sandocap.sirfelius.tsundoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     /** Activity Log String */
-    public static final String LOG_TAG = MainActivity.class.getName();
+    static final String LOG_TAG = MainActivity.class.getName();
+
+    /** Mock string for API request */
+    static final String API_QUERY_MOCK =
+            "https://www.googleapis.com/books/v1/volumes?q=harry+inauthor:rowling&results=1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Make sure that TextView works
+        TextView textView = (TextView) findViewById(R.id.simple_text_view);
+        textView.setText(API_QUERY_MOCK);
     }
 }
