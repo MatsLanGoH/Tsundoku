cf9b7ce823e08309cd67d9157b77c26fce140aa3
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2017-06-17 23:40:43 +0900

    Added basic View for a Book object.

--- app/src/main/java/com/sandocap/sirfelius/tsundoku/BookAdapter.java
+++ app/src/main/java/com/sandocap/sirfelius/tsundoku/BookAdapter.java
@@ -3,9 +3,11 @@ package com.sandocap.sirfelius.tsundoku;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -43,6 +45,31 @@ class BookAdapter extends ArrayAdapter<Book> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+
+        // Check if the existing View is being reused, otherwise inflate the View.
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.book_item, parent, false);
+        }
+
+        // Get the {@link Book} object located at this position in the list.
+        Book currentBook = getItem(position);
+
+        if (currentBook != null) {
+            // TODO: Implement book covers!
+
+            // Find the TextView in the book_item.xml with the ID book_title.
+            TextView bookTitleView = (TextView) listItemView.findViewById(R.id.book_title);
+
+            // Get book title for current book.
+            String bookTitle = currentBook.getTitle();
+
+            // Display the title of the current book in that TextView.
+            bookTitleView.setText(bookTitle);
+
+        }
+
+        return listItemView;
     }
 }
